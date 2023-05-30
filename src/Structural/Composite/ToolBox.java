66ac636b2d58194031c8d9037e7d4c0d0b9f3c33
Oleksandr Lesyk <sasha.lesik.98@gmail.com>
2023-05-31 01:10:10 +0300

    change composite

--- src/Structural/Composite/ToolBox.java
+++ src/Structural/Composite/ToolBox.java
@@ -1,10 +1,14 @@
 package Structural.Composite;
 
+import java.io.Console;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ToolBox implements Tool {
     private List<Tool> tools = new ArrayList<>();
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_GREEN = "\u001B[32m";
 
     public void addTool(Tool tool){
         tools.add(tool);
@@ -16,14 +20,13 @@ public class ToolBox implements Tool {
 
     @Override
     public void take() {
-        System.out.println("- - - I`m take tool box with tools inside");
+        System.out.println("I`m take tool box with tools inside");
         for (Tool tool : tools){
             if (tool instanceof ToolBox){
-                System.out.println("\t- - - - I`m have tool box inside me - - - -");
+                System.out.println(ANSI_GREEN + "\t I`m have tool box inside me - - - - \n\t{" + ANSI_RESET);
             }
             tool.take();
         }
-        System.out.println("- - - Tool box is empty");
-
+        System.out.println(ANSI_YELLOW + "\t} \nTool box is empty" + ANSI_RESET);
     }
 }
